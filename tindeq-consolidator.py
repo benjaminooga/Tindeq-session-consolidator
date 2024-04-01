06bdb7d318a447b789cfc09beb6cb372a77c6d6e
Benjamin <benjamin.robinson@gmail.com>
2024-04-01 16:00:16 -0400

    Added commandline options to specify output formats

--- tindeq-consolidator.py
+++ tindeq-consolidator.py
@@ -1,22 +1,48 @@
 '''
 Assumes info.csv structure: date,tag,comment,unit,reps,work dur.,pause btw. reps,sets,pause btw. sets,type
 
-Uses the save name of your session as the exercise name
+Uses the saved name of your session as the exercise name. Details of the set (reps, work, and rest) are saved with each record. 
+Note that reset between sets is not included.
 
+Whether your saved sessions have one or multiple sets, the script also looks for follow-up sets, so your sets in the output may differ
+from within the Tindeq app if you have a mix of sessions on a day. This is so analysis is easier in that you don't always have to look 
+at the time and compare against other records.
+
+Inputs:
+    Assumes that the zip file contains zipped sessions. Only tested with Repeaters data.
 Outputs:
     1. CSV file containing all exported sessions
-    2. Chart showing the max Peaks and average of Averages per day
-
+    2. JSON file containing all exported sessions
 '''
 from zipfile import ZipFile
-import argparse, sys
+import argparse
 import pandas as pd
 import AccountKeys
+import os
 
 PATH = AccountKeys.KEYS['TINDEQ_PATH']
 
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTIONS]",
+        description='-'*80 + '\nTindeq Session Consolidator\n' + '-'*80 + '\n'
+    )
+    parser.add_argument("filename", type=str, help="Zip file to convert")
+    parser.add_argument("-c", "--csv", action='store_true', help="Ouput CSV (default)")
+    parser.add_argument("-j", "--json", action='store_true', help="Output JSON")
+    parser.add_argument("-v", "--version", action="version", version=f"{parser.prog} version 1.0.0")
+    return parser
+
+def check_path(filename_or_path):
+    if os.path.isabs(filename_or_path):
+        # Indicates full path
+        return 1
+
+    # Just a file
+    return 0
+
 def consolidator(zipfilename):
-    ''' '''
+    ''' Take a zip file and return a dataframe of all nested zip contents '''
     tindeq_df = pd.DataFrame()
 
     # Unzip main zip, process sessions within
@@ -96,20 +122,33 @@ def consolidator(zipfilename):
 
     return tindeq_df
 
-
 def main():
-    # zipfilename = PATH + 'tindeq.zip'
-    zipfilename = 'tindeq0331.zip'
-
-    tindeq_df = consolidator(PATH + zipfilename)
-
-    # TODO: Output as CSV, JSON, both
-    tindeq_df.to_csv(PATH + zipfilename.split('.')[0] + '.csv')
+    parser = init_argparse()
+    args = parser.parse_args()
     
-    pretty_json = tindeq_df.T.to_json(PATH + zipfilename.split('.')[0] + '.json', indent=4)
+    if check_path(args.filename) == 0:
+        output_dir = os.getcwd() + '/'
+    else:
+        output_dir = ''
     
-    # with open(PATH + zipfilename.split('.')[0] + '.json', 'w') as jfile:
-    #     jfile.write(pretty_json)
+    if args.filename:
+        print(f"[+] INFO: Consolidating '{output_dir + args.filename}'")
+    else:
+        print(f"[+] INFO: File not specified, looking for 'tindeq.zip'")
+        args.filename = 'tindeq.zip'
+
+    zipfilename = args.filename
+
+    tindeq_df = consolidator(zipfilename)
+
+    # Write the output
+    if args.csv or (args.csv == False and args.json == False):
+        print(f"[+] INFO: Writing CSV '{output_dir + zipfilename.split('.')[0] + '.csv'}'")
+        tindeq_df.to_csv(output_dir + zipfilename.split('.')[0] + '.csv')
+    
+    if args.json:
+        print(f"[+] INFO: Writing JSON '{output_dir + zipfilename.split('.')[0] + '.json'}'")
+        pretty_json = tindeq_df.T.to_json(output_dir + zipfilename.split('.')[0] + '.json', indent=4)
 
 if __name__ == "__main__":
     main()
